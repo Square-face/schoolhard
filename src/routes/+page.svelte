c65cfd5b546bbb0c09fc4e54690bcdbba0648e97
Square-face <linus.michelsson@gmail.com>
2024-04-12 11:24:43 +0000

    Init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+  import Greet from '../lib/Greet.svelte'
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<Greet />
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
