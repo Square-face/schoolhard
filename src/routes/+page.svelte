e44199928f88e242571da11c9007fd44bd5b65bc
Square-face <linus.michelsson@gmail.com>
2024-04-16 11:41:01 +0000

    Created login route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,2 @@
-<script>
-  import Greet from '../lib/Greet.svelte'
-</script>
-
-<h1>Welcome to SvelteKit</h1>
-<Greet />
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Schoolhard</h1>
+<a href="/login">Login</a>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    let school = "";
+    let username = "";
+    let password = "";
+</script>
+
+<div class="login">
+	<div class="form">
+        <label for="school">School</label>
+		<input id="school" type="text" bind:value={school}/>
+
+        <label for="username">Username</label>
+		<input id="username" type="text" bind:value={username}/>
+
+        <label for="password">Password</label>
+		<input id="password" type="password" bind:value={password}/>
+
+        <button>Login</button>
+	</div>
+</div>
+
+<style lang="scss">
+    .login {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+
+        .form {
+            display: flex;
+            flex-direction: column;
+        }
+    }
+</style>
