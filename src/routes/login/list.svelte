55d627ca8ea4fe8af69bcbb89af79526c0ea7121
Square-face <linus.michelsson@gmail.com>
2024-04-17 15:04:25 +0000

    Prettier search bar and added toggle for displaying unsupported schools

--- src/routes/login/list.svelte
+++ src/routes/login/list.svelte
@@ -22,15 +22,28 @@
 		});
 	}
 
+    async function toggle_unsupported() {
+        include_unsupported = !include_unsupported;
+        apply_filters();
+    }
+
 	refresh();
 	apply_filters();
 </script>
 
 <div class="school-list">
 	<h2>Select school</h2>
-    <button on:click={refresh}>Refresh</button>
-	<input type="text" bind:value={query} on:keyup={apply_filters} />
-	<div>
+	<div class="filters">
+        <input type="checkbox" checked={include_unsupported} on:click={toggle_unsupported}>
+		<input
+			class="large-input"
+			type="text"
+			placeholder="Type to search"
+			bind:value={query}
+			on:keyup={apply_filters}
+		/>
+	</div>
+	<div class="list">
 		{#each schools as school}
 			<Listing
 				{school}
@@ -52,15 +65,47 @@
 		border: 1px solid black;
 		height: 100%;
 		overflow: hidden;
+        padding: 1em;
 
-		div {
-            display: flex;
-            flex-direction: column;
+		div.filters {
+			display: flex;
+			gap: 1em;
+            padding: 0 1em;
+			width: 100%;
+
+            input[type='checkbox'] {
+                height: 3em;
+                width: 3em;
+                background: red;
+                border: 10px solid black;
+            }
+
+			input[type='text'] {
+				width: 100%;
+				padding: 1em 1em;
+				border-radius: 0.8em;
+				border: 2px solid gray;
+				box-shadow: 0 0 5px gray;
+			}
+
+			// button {
+			// 	background: white;
+			// 	padding: 0;
+			// 	font-size: 1.5em;
+			// 	border-radius: 2em;
+			// 	border: 2px solid gray;
+			// 	box-shadow: 0 0 5px gray;
+			// }
+		}
+
+		div.list {
+			display: flex;
+			flex-direction: column;
 			height: 100%;
 			overflow: scroll;
-            margin: 0;
-            padding: 10px;
-            gap: 10px;
+			margin: 0;
+			padding: 1em;
+			gap: 10px;
 		}
 	}
 </style>
