b398856da84562f3d17fcebeaab103735534dfe6
Square-face <linus.michelsson@gmail.com>
2024-04-19 20:38:22 +0200

    Added missing dependencies & bumped tauri to beta.15

--- flake.nix
+++ flake.nix
@@ -10,26 +10,28 @@
         pkgs = nixpkgs.legacyPackages.${system};
 
         libraries = with pkgs;[
-            webkitgtk
+            webkitgtk_4_1
             gtk3
             cairo
             gdk-pixbuf
-            glib
-            dbus
-            openssl_3
+            glib.out
+            dbus.lib
+            openssl_3.out
         ];
 
         packages = with pkgs; [
             cargo
             rustc
+            rustup
             pkg-config
             dbus
             openssl_3
             glib
             gtk3
-            libsoup
+            libsoup_3
             webkitgtk_4_1
             appimagekit
+            llvmPackages.bintools
         ];
     in
     {
